955e9297b142eac7e880246261325d1e98bcbf8d
Pieter Passmann <pieter.passmann@gmail.com>
2021-12-19 07:34:21 +0100

    animation model struct. need to start somewhere...

---
+++ src/model/Animation/Solid.js
@@ -0,0 +1,7 @@
+import Animation from "../Animation.js";
+
+export default class Solid extends Animation {
+  constructor() {
+    super();
+  }
+}
---
+++ src/model/Socket.js
@@ -0,0 +1,11 @@
+export default class Socket {
+  constructor() {
+    this.id = null;
+    this.ip = null;
+    this.universe = null;
+    this.net = null;
+    this.subnet = null;
+    const socket = null;
+    this.instance = socket;
+  }
+}
